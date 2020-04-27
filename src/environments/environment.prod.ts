92be5a7c43ae5496accba9a1a59801ae9fe42d87
Jaxon Burningham <jaxonburn@gmail.com>
2020-04-27 09:05:59 -0600

    c

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCh62gSc8LtYsDif7c7T3sURQVtQ4NtCc4",
+    authDomain: "mtechflix-debfc.firebaseapp.com",
+    databaseURL: "https://mtechflix-debfc.firebaseio.com",
+    projectId: "mtechflix-debfc",
+    storageBucket: "mtechflix-debfc.appspot.com",
+    messagingSenderId: "95989641135",
+    appId: "1:95989641135:web:8909ae026c137d0cb4095a"
+  }
 };
